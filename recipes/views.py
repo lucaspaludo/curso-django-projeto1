7fdcb64b62316ee75f19adeb265ccdf72c4a3e4c
Lucas Boarim Paludo <lucaspaludo2018@gmail.com>
2023-12-18 11:06:00 -0300

    adicionando header recipes

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 #HTTP Request
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Lucas Paludo',
     })
 
